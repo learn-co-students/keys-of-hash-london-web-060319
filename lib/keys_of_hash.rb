c32ed96a8aa2fed758e13759421d54a75dadd0bf
louisenorris <louisenorris73@yahoo.co.uk>
2019-05-06 00:54:21 +0100

    Completed Keys Of Hash lab

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,22 @@
+#animals = {
+	#"sugar glider"=>"Australia",
+	#"aye-aye"=> "Madagascar",
+	#"red-footed tortoise"=>"Panama",
+	#"kangaroo"=> "Australia",
+	#"tomato frog"=>"Madagascar",
+	#"koala"=>"Australia"}
+    
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+	def keys_of(*arguments) 
+	  array = []
+	  self.each do |key, value| #(self keyword gives access to the current object)
+	  	arguments.each do |i|
+	  		if i == value
+	  			array.push(key)
+	  		end
+   		 end
+	  end
+	  return array
+	end
 end
